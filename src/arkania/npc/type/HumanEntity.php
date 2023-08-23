b2bc4ddd7559f773042e8492ef6ffbaecfc8274f
Dumont-Julien <dumontj357@gmail.com>
2023-08-23 22:57:34 +0200

    (feat): Implementation d'un système de NPC

---
+++ src/arkania/npc/type/HumanEntity.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *     _      ____    _  __     _      _   _   ___      _             __     __  ____
+ *    / \    |  _ \  | |/ /    / \    | \ | | |_ _|    / \            \ \   / / |___ \
+ *   / _ \   | |_) | | ' /    / _ \   |  \| |  | |    / _ \    _____   \ \ / /    __) |
+ *  / ___ \  |  _ <  | . \   / ___ \  | |\  |  | |   / ___ \  |_____|   \ V /    / __/
+ * /_/   \_\ |_| \_\ |_|\_\ /_/   \_\ |_| \_| |___| /_/   \_\            \_/    |_____|
+ *
+ * @author: Julien
+ * @link: https://github.com/ArkaniaStudios
+ */
+
+namespace arkania\npc\type;
+
+use arkania\npc\base\CustomEntity;
+use pocketmine\entity\Location;
+use pocketmine\entity\Skin;
+use pocketmine\nbt\tag\CompoundTag;
+
+class HumanEntity extends CustomEntity {
+
+    public function __construct(Location $location, Skin $skin, ?CompoundTag $nbt = null) {
+        parent::__construct($location, $skin, $nbt);
+    }
+}
